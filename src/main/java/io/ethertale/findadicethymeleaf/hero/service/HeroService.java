ca5f46916f05a42b77207bbb4776837a734d11f0
Galin Yanakov <galin@ivel.in>
2025-07-23 23:48:34 +0300

    Remove: Custom hero creating

--- src/main/java/io/ethertale/findadicethymeleaf/hero/service/HeroService.java
+++ src/main/java/io/ethertale/findadicethymeleaf/hero/service/HeroService.java
@@ -23,37 +23,6 @@ public class HeroService {
     }
 
     public void createHero() {
-//        Hero hero = Hero.builder()
-//                .id(UUID.randomUUID())
-//                .name("Hero")
-//                .age(1)
-//                .gender(Genders.MALE)
-//                .charClass(Classes.ARTIFICER)
-//                .alignment(Alignment.CHAOTIC_EVIL)
-//                .background(Backgrounds.ATHLETE)
-//                .strength(1)
-//                .dexterity(1)
-//                .constitution(1)
-//                .intelligence(1)
-//                .wisdom(1)
-//                .charisma(1)
-//                .createdAt(LocalDateTime.now())
-//                .build();
-        Hero hero = new Hero();
-        hero.setId(UUID.randomUUID());
-        hero.setName("hero");
-        hero.setAge(1);
-        hero.setGender(Genders.MALE);
-        hero.setCharClass(Classes.ARTIFICER);
-        hero.setAlignment(Alignment.CHAOTIC_EVIL);
-        hero.setBackground(Backgrounds.ATHLETE);
-        hero.setStrength(1);
-        hero.setDexterity(1);
-        hero.setConstitution(1);
-        hero.setConstitution(1);
-        hero.setIntelligence(1);
-        hero.setWisdom(1);
-        hero.setCharisma(1);
-        hero.setCreatedAt(LocalDateTime.now());
+
     }
 }
