de560bbbe33a5daca69988e24cb21b2d83895ed9
Galin Yanakov <galin@ivel.in>
2025-07-29 20:10:25 +0300

    Add: security configured and now users can login and register

---
+++ src/main/java/io/ethertale/findadicethymeleaf/security/AuthenticationDetails.java
@@ -0,0 +1,157 @@
+package io.ethertale.findadicethymeleaf.security;
+
+import io.ethertale.findadicethymeleaf.hero.model.Hero;
+import io.ethertale.findadicethymeleaf.user.model.UserRoles;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+public class AuthenticationDetails implements UserDetails {
+
+    private UUID id;
+    private String username;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String description;
+    private UserRoles role;
+    private Hero hero;
+    private String imageUrl;
+    private LocalDateTime createdAt;
+
+    public AuthenticationDetails(UUID id, String username, String password, String email, String firstName, String lastName, String description, UserRoles role, Hero hero, String imageUrl, LocalDateTime createdAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.description = description;
+        this.role = role;
+        this.hero = hero;
+        this.imageUrl = imageUrl;
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority("ROLE_" + role.name());
+        return List.of(simpleGrantedAuthority);
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public UserRoles getRole() {
+        return role;
+    }
+
+    public void setRole(UserRoles role) {
+        this.role = role;
+    }
+
+    public Hero getHero() {
+        return hero;
+    }
+
+    public void setHero(Hero hero) {
+        this.hero = hero;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
--- src/main/java/io/ethertale/findadicethymeleaf/user/service/UserService.java
+++ src/main/java/io/ethertale/findadicethymeleaf/user/service/UserService.java
@@ -1,7 +1,56 @@
 package io.ethertale.findadicethymeleaf.user.service;
 
+import io.ethertale.findadicethymeleaf.security.AuthenticationDetails;
+import io.ethertale.findadicethymeleaf.user.model.User;
+import io.ethertale.findadicethymeleaf.user.model.UserRoles;
+import io.ethertale.findadicethymeleaf.user.repo.UserRepo;
+import io.ethertale.findadicethymeleaf.web.dto.RegisterDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+
+    private final UserRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepo userRepo, PasswordEncoder passwordEncoder) {
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void registerUser(RegisterDTO registerDTO) {
+        User user = new User();
+        user.setUsername(registerDTO.getUsername());
+        user.setPassword(passwordEncoder.encode(registerDTO.getPassword()));
+        user.setEmail(registerDTO.getEmail());
+        user.setFirstName(registerDTO.getFirstName());
+        user.setLastName(registerDTO.getLastName());
+        user.setRole(UserRoles.USER);
+        user.setCreatedAt(LocalDateTime.now());
+
+        userRepo.save(user);
+    }
+
+    public User getUserById(UUID id) {
+        if (userRepo.findById(id).isEmpty()) {
+            throw new UsernameNotFoundException(id.toString());
+        }
+        return userRepo.findById(id).get();
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        return new AuthenticationDetails(user.getId(), user.getUsername(), user.getPassword(), user.getEmail(), user.getFirstName(), user.getLastName(), user.getDescription(), user.getRole(), user.getHero(), user.getImageUrl(), user.getCreatedAt());
+    }
 }
