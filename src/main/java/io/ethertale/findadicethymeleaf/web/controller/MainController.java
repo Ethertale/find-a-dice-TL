e507943620b7ab95b4b5f6334cf4be937ccc7f1f
Galin Yanakov <galin@ivel.in>
2025-07-29 20:10:44 +0300

    Update: endpoints configured for registration and login

---
+++ src/main/java/io/ethertale/findadicethymeleaf/web/controller/MainController.java
@@ -0,0 +1,65 @@
+package io.ethertale.findadicethymeleaf.web.controller;
+
+import io.ethertale.findadicethymeleaf.security.AuthenticationDetails;
+import io.ethertale.findadicethymeleaf.user.model.User;
+import io.ethertale.findadicethymeleaf.user.service.UserService;
+import io.ethertale.findadicethymeleaf.web.dto.LoginDTO;
+import io.ethertale.findadicethymeleaf.web.dto.RegisterDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    private final UserService userService;
+
+    @Autowired
+    public MainController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public ModelAndView home(){
+        return new ModelAndView("welcomePage");
+    }
+
+    @GetMapping("/home")
+    public ModelAndView homePage(@AuthenticationPrincipal AuthenticationDetails authenticationDetails){
+
+        User loggedUser = userService.getUserById(authenticationDetails.getId());
+
+        ModelAndView mav = new ModelAndView("home");
+        mav.addObject("loggedUser", loggedUser);
+
+        return mav;
+    }
+
+    @GetMapping("/register")
+    public ModelAndView register(){
+        ModelAndView mav = new ModelAndView("register");
+        mav.addObject("registerDTO", new RegisterDTO());
+        return mav;
+    }
+
+    @PostMapping("/register")
+    public String registerProfile(RegisterDTO registerDTO){
+        userService.registerUser(registerDTO);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public ModelAndView login(){
+        ModelAndView mav = new ModelAndView("login");
+        mav.addObject("loginDTO", new LoginDTO());
+        return mav;
+    }
+}
