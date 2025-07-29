bba592c3e67120652efe0b20c4a9c86722e301b9
Galin Yanakov <galin@ivel.in>
2025-07-30 00:08:19 +0300

    Update: added Slf4j to the UserService to track information on the backend

--- src/main/java/io/ethertale/findadicethymeleaf/user/service/UserService.java
+++ src/main/java/io/ethertale/findadicethymeleaf/user/service/UserService.java
@@ -5,6 +5,7 @@ import io.ethertale.findadicethymeleaf.user.model.User;
 import io.ethertale.findadicethymeleaf.user.model.UserRoles;
 import io.ethertale.findadicethymeleaf.user.repo.UserRepo;
 import io.ethertale.findadicethymeleaf.web.dto.RegisterDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class UserService implements UserDetailsService {
 
@@ -50,6 +52,8 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        log.info("At: {} User with ID: {} - and Username: {} logged in.", LocalDateTime.now(), user.getId(), username);
 
         return new AuthenticationDetails(user.getId(), user.getUsername(), user.getPassword(), user.getEmail(), user.getFirstName(), user.getLastName(), user.getDescription(), user.getRole(), user.getHero(), user.getImageUrl(), user.getCreatedAt());
     }
