9cdf095f1881db1867ce47b8413beb9998136d67
Galin Yanakov <galin@ivel.in>
2025-08-04 14:11:17 +0300

    Update: HeroService now creates a dummy hero for the user.

--- src/main/java/io/ethertale/findadicethymeleaf/hero/service/HeroService.java
+++ src/main/java/io/ethertale/findadicethymeleaf/hero/service/HeroService.java
@@ -6,11 +6,11 @@ import io.ethertale.findadicethymeleaf.hero.model.Classes;
 import io.ethertale.findadicethymeleaf.hero.model.Hero;
 import io.ethertale.findadicethymeleaf.hero.repo.HeroRepo;
 import io.ethertale.findadicethymeleaf.user.model.Genders;
+import io.ethertale.findadicethymeleaf.user.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Service
 public class HeroService {
@@ -22,7 +22,27 @@ public class HeroService {
         this.heroRepo = heroRepo;
     }
 
-    public void createHero() {
+    public Hero createFirstHero(User user) {
+        Hero hero = Hero.builder()
+                .user(user)
+                .name("My Hero")
+                .age(18)
+                .gender(Genders.MALE)
+                .charClass(Classes.ARTIFICER)
+                .description("This is my description")
+                .imageUrl("https://i.ibb.co/WWDv4mYx/Logo-Transparent.png")
+                .alignment(Alignment.TRUE_NEUTRAL)
+                .background(Backgrounds.ATHLETE)
+                .strength(1)
+                .dexterity(1)
+                .constitution(1)
+                .intelligence(1)
+                .wisdom(1)
+                .charisma(1)
+                .createdAt(LocalDateTime.now())
+                .build();
 
+        heroRepo.save(hero);
+        return hero;
     }
 }
