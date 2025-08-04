e5cc42aaefddf6779fec1b552ae763481df3dd68
Galin Yanakov <galin@ivel.in>
2025-08-04 14:10:45 +0300

    Add: comment class

---
+++ src/main/java/io/ethertale/findadicethymeleaf/post/model/Comment.java
@@ -0,0 +1,48 @@
+package io.ethertale.findadicethymeleaf.post.model;
+
+import io.ethertale.findadicethymeleaf.hero.model.Hero;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "comment")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "hero_id", nullable = false)
+    private Hero hero;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(nullable = false)
+    private String content;
+
+    public String getTimestamp() {
+        return createdAt.getDayOfMonth()
+                + " "
+                + createdAt.getMonth().toString()
+                + " "
+                + createdAt.getYear()
+                + ", at "
+                + createdAt.getHour()
+                + " "
+                + createdAt.getMinute();
+    }
+}
