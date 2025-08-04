4fa4dc19869a480d6599633bd22cbf83aeef08a9
Galin Yanakov <galin@ivel.in>
2025-08-04 14:12:11 +0300

    Update: repo

--- src/main/java/io/ethertale/findadicethymeleaf/user/repo/UserRepo.java
+++ src/main/java/io/ethertale/findadicethymeleaf/user/repo/UserRepo.java
@@ -1,5 +1,6 @@
 package io.ethertale.findadicethymeleaf.user.repo;
 
+import io.ethertale.findadicethymeleaf.security.AuthenticationDetails;
 import io.ethertale.findadicethymeleaf.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,6 @@ import java.util.UUID;
 @Repository
 public interface UserRepo extends JpaRepository<User, UUID> {
     Optional<User> findByUsername(String username);
+
+    User getUserByUsername(String username);
 }
