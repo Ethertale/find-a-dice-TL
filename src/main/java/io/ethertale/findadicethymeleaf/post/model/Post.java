0615579f1fe4fb4e353db52cbb002a4eb21dcdab
Galin Yanakov <galin@ivel.in>
2025-08-04 14:10:37 +0300

    Add: post class

---
+++ src/main/java/io/ethertale/findadicethymeleaf/post/model/Post.java
@@ -0,0 +1,57 @@
+package io.ethertale.findadicethymeleaf.post.model;
+
+import io.ethertale.findadicethymeleaf.hero.model.Hero;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "post")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "hero_id", nullable = false)
+    private Hero hero;
+
+    @OneToMany(mappedBy = "post")
+    private Set<Comment> comments;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column
+    private String image;
+
+    @Column(nullable = false)
+    private int likes;
+
+    public String getTimestamp() {
+        return createdAt.getDayOfMonth()
+                + " "
+                + createdAt.getMonth().toString()
+                + " "
+                + createdAt.getYear()
+                + ", at "
+                + createdAt.getHour()
+                + " "
+                + createdAt.getMinute();
+    }
+}
