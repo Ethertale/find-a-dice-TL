affd33fc8d1d60590b48395ca86d21ee0d8a1aae
Galin Yanakov <galin@ivel.in>
2025-08-05 11:24:12 +0300

    Add: DTO for the hero update function

---
+++ src/main/java/io/ethertale/findadicethymeleaf/web/dto/HeroUpdateDTO.java
@@ -0,0 +1,31 @@
+package io.ethertale.findadicethymeleaf.web.dto;
+
+import io.ethertale.findadicethymeleaf.hero.model.Alignment;
+import io.ethertale.findadicethymeleaf.hero.model.Backgrounds;
+import io.ethertale.findadicethymeleaf.hero.model.Classes;
+import io.ethertale.findadicethymeleaf.user.model.Genders;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HeroUpdateDTO {
+    private String name;
+    private int age;
+    private Genders gender;
+    private Classes charClass;
+    private String description;
+    private String imageUrl;
+    private Alignment alignment;
+    private Backgrounds background;
+    private int strength;
+    private int dexterity;
+    private int constitution;
+    private int intelligence;
+    private int wisdom;
+    private int charisma;
+}
