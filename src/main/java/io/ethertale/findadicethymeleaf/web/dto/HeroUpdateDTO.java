7a4b7593b04202f62fcdf779c52c517a7d63f117
Galin Yanakov <galin@ivel.in>
2025-08-06 14:20:22 +0300

    Update: adding an autopopulator for the DTO, so I can reuse the already populated info from the DB back into the editHero form

--- src/main/java/io/ethertale/findadicethymeleaf/web/dto/HeroUpdateDTO.java
+++ src/main/java/io/ethertale/findadicethymeleaf/web/dto/HeroUpdateDTO.java
@@ -1,9 +1,6 @@
 package io.ethertale.findadicethymeleaf.web.dto;
 
-import io.ethertale.findadicethymeleaf.hero.model.Alignment;
-import io.ethertale.findadicethymeleaf.hero.model.Backgrounds;
-import io.ethertale.findadicethymeleaf.hero.model.Classes;
-import io.ethertale.findadicethymeleaf.hero.model.Races;
+import io.ethertale.findadicethymeleaf.hero.model.*;
 import io.ethertale.findadicethymeleaf.user.model.Genders;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -31,4 +28,27 @@ public class HeroUpdateDTO {
     private int intelligence;
     private int wisdom;
     private int charisma;
+
+    public static HeroUpdateDTO populateInfoHero(Hero hero) {
+        //Set a default DTO populated with the heroes stats
+        HeroUpdateDTO dto = new HeroUpdateDTO();
+
+        dto.setName(hero.getName());
+        dto.setAge(hero.getAge());
+        dto.setRace(hero.getRace());
+        dto.setGender(hero.getGender());
+        dto.setCharClass(hero.getCharClass());
+        dto.setLevel(hero.getLevel());
+        dto.setDescription(hero.getDescription());
+        dto.setImageUrl(hero.getImageUrl());
+        dto.setAlignment(hero.getAlignment());
+        dto.setBackground(hero.getBackground());
+        dto.setStrength(hero.getStrength());
+        dto.setDexterity(hero.getDexterity());
+        dto.setConstitution(hero.getConstitution());
+        dto.setIntelligence(hero.getIntelligence());
+        dto.setWisdom(hero.getWisdom());
+        dto.setCharisma(hero.getCharisma());
+        return dto;
+    }
 }
