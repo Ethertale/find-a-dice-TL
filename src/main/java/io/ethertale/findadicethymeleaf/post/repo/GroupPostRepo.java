2eb533780b7de3fea1acc8f27a093d0c1f76c0fa
Galin Yanakov <galin@ivel.in>
2025-08-12 13:17:17 +0300

    Add: repo for the GroupPost

--- src/main/java/io/ethertale/findadicethymeleaf/post/repo/GroupPostRepo.java
+++ src/main/java/io/ethertale/findadicethymeleaf/post/repo/GroupPostRepo.java
@@ -1,9 +1,9 @@
 package io.ethertale.findadicethymeleaf.post.repo;
 
-import io.ethertale.findadicethymeleaf.post.model.Post;
+import io.ethertale.findadicethymeleaf.post.model.GroupPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface PostRepo extends JpaRepository<Post, UUID> {
+public interface GroupPostRepo extends JpaRepository<GroupPost, UUID> {
 }
